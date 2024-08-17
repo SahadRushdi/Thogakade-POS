47d42436eec1390202f6e777340d4ddac9876859
javaprocoder <160300225+javaprocoder@users.noreply.github.com>
2024-08-17 20:45:49 +0530

    Introduced Dash Board And View Customer Table Form and finished setting customer table

--- src/main/java/controller/ViewFormController.java
+++ src/main/java/controller/ViewFormController.java
@@ -1,5 +1,65 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import db.DBConnection;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import model.Customer;
+
+import java.util.List;
+
 public class ViewFormController {
+
+    @FXML
+    private ComboBox<?> cmbTitle;
+
+    @FXML
+    private TableColumn<?, ?> colAddress;
+
+    @FXML
+    private TableColumn<?, ?> colDob;
+
+    @FXML
+    private TableColumn<?, ?> colID;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colNumber;
+
+    @FXML
+    private DatePicker dateDob;
+
+    @FXML
+    private TableView<Customer> tblCustomers;
+
+    @FXML
+    private JFXTextField txtAddress;
+
+    @FXML
+    private JFXTextField txtId;
+
+    @FXML
+    private JFXTextField txtName;
+
+    @FXML
+    private JFXTextField txtNumber;
+
+    @FXML
+    void btnReloadOnAction(ActionEvent event) {
+        List<Customer> customerList = DBConnection.getInstance().getConnection();
+        ObservableList<Customer> customerObservableList = FXCollections.observableArrayList();
+
+        customerList.forEach(obj -> {
+            customerObservableList.add(obj);
+        });
+    }
 
 }
