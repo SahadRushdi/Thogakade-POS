b8421b446d1d5a8323419d4c63b367039d7a8997
javaprocoder <160300225+javaprocoder@users.noreply.github.com>
2024-08-17 19:38:38 +0530

    Added Titles and created Add Customer Form

--- src/main/java/controller/AddCustomerFormController.java
+++ src/main/java/controller/AddCustomerFormController.java
@@ -1,4 +1,47 @@
 package controller;
 
-public class AddCustomerFormController {
+import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddCustomerFormController implements Initializable {
+
+    @FXML
+    private ComboBox<String> cmbTitle;
+
+    @FXML
+    private DatePicker dateDob;
+
+    @FXML
+    private JFXTextField txtAddress;
+
+    @FXML
+    private JFXTextField txtId;
+
+    @FXML
+    private JFXTextField txtName;
+
+    @FXML
+    private JFXTextField txtNumber;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ObservableList<String> titles = FXCollections.observableArrayList();
+        titles.add("Mr.");
+        titles.add("Miss.");
+        cmbTitle.setItems(titles);
+    }
 }
