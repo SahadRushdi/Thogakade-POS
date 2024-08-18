7a673e3668cd82fb226afcec695b6618f0f31fba
javaprocoder <160300225+javaprocoder@users.noreply.github.com>
2024-08-18 09:16:26 +0530

    Addjusted Title

--- src/main/java/model/Customer.java
+++ src/main/java/model/Customer.java
@@ -2,18 +2,27 @@ package model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.time.LocalDate;
 
 @Data
-@AllArgsConstructor
 @ToString
+@NoArgsConstructor
 public class Customer {
     private String id;
     private String name;
     private String address;
     private String number;
-    private String title;
     private LocalDate dob;
+
+    public Customer(String id, String name, String address, String number, String title, LocalDate dob) {
+        this.id = id;
+        this.name = title+name;
+        this.address = address;
+        this.number = number;
+        this.dob = dob;
+    }
+
 }
